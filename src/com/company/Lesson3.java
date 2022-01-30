516d00d9503c5b2d2ff8ef0af77e8d1960393639
shh <vi950424@gmail.com>
2022-01-31 01:04:31 +0300

    25

--- src/com/company/Lesson3.java
+++ src/com/company/Lesson3.java
@@ -23,36 +23,34 @@ public class Lesson3 {
         loops();
     }
 
-        public static void comparisonsOperators(int num){
-            if ( num != 8 && num != 1 && num != 9) {
-                System.out.println("число не равно 1, 8, 9");
-            }
-            else if ((num == 8 || num == 1 || num == 9)) {
-                System.out.println("число равно или 1 или 8 или 9");
-            }
-
-            if (num == 0 || num == 1) {
-                System.out.println("число равно 1 или 0");
-            }
-            else {
-                System.out.println("число не равно ни 1, ни 0,");
-            }
+    public static void comparisonsOperators(int num) {
+        if (num != 8 && num != 1 && num != 9) {
+            System.out.println("число не равно 1, 8, 9");
+        } else if ((num == 8 || num == 1 || num == 9)) {
+            System.out.println("число равно или 1 или 8 или 9");
         }
 
-        public static void loops(){
+        if (num == 0 || num == 1) {
+            System.out.println("число равно 1 или 0");
+        } else {
+            System.out.println("число не равно ни 1, ни 0,");
+        }
+    }
+
+    public static void loops() {
         int counter = 1;
-        while (counter < 100){
+        while (counter < 100) {
             counter++;
             System.out.println(counter);
         }
         int year = 85;
-        while (year >= 0){
-            if (year % 4 == 0){
-                System.out.println("Високосный год"+ year);
+        while (year >= 0) {
+            if (year % 4 == 0) {
+                System.out.println("Високосный год" + year);
             }
             year--;
         }
-        }
     }
+}
 
 
