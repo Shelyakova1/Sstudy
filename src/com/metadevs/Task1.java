5a876396aa8194b9db7b231975815e57cec9e306
shh <vi950424@gmail.com>
2022-02-03 18:10:23 +0300

    task from paladins firs comm1  review

---
+++ src/com/metadevs/Task1.java
@@ -0,0 +1,31 @@
+package com.metadevs;
+import java.util.*;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        test();
+        scan.close();
+    }
+
+    public static void test() {
+        Scanner scan1 = new Scanner(System.in);
+        int i = scan1.nextInt();
+        double d = scan1.nextDouble();
+        String s = scan1.nextLine();
+        scan1.close();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+    }
+}
+
---
+++ src/com/metadevs/Task2.java
@@ -0,0 +1,15 @@
+package com.metadevs;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        int value1 = scann.nextInt();
+        int value2 = scann.nextInt();
+        System.out.println("Sum " + (value1 + value2));
+        System.out.println("Dif " + (value1 - value2));
+        System.out.println("Quo " + (value1 * value2));
+        System.out.println("Prod " + (value1 / value2));
+        scann.close();
+    }
+}
