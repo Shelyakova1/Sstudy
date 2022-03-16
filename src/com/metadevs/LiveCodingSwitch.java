8b35026ac87996e5594e07d157adec7e0a090356
shh <vi950424@gmail.com>
2022-03-16 20:06:02 +0300

    live coding swich case 2 comm

--- src/com/metadevs/LiveCodingSwitch.java
+++ src/com/metadevs/LiveCodingSwitch.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 
 public class LiveCodingSwitch {
 
-    final static int firstDec = 1;
-    final static int secondDec = 2;
-    final static int thierdDec = 3;
-    final static int fourtDec = 4;
+    final static int FIRST_DECLEMINATION = 1;
+    final static int SECOND_DECLEMINATION = 2;
+    final static int THIERD_DECLEMINATION = 3;
+    final static int FOURT_DECLEMINATION = 4;
     static Scanner scanAge = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         System.out.println("Введите число.");
         int k = scanAge.nextInt();
         int x;
         x = k % 10;
         switch (x) {
-            case firstDec:
+            case FIRST_DECLEMINATION:
                 switch (k) {
                     case 11:
                         System.out.println("Мне " + k + " лет");
@@ -26,7 +25,7 @@ public class LiveCodingSwitch {
                         break;
                 }
                 break;
-            case secondDec:
+            case SECOND_DECLEMINATION:
                 switch (k) {
                     case 12:
                         System.out.println("Мне " + k + " лет");
@@ -36,7 +35,7 @@ public class LiveCodingSwitch {
                         break;
                 }
                 break;
-            case thierdDec:
+            case THIERD_DECLEMINATION:
                 switch (k) {
                     case 13:
                         System.out.println("Мне " + k + " лет");
@@ -46,7 +45,7 @@ public class LiveCodingSwitch {
                         break;
                 }
                 break;
-            case fourtDec:
+            case FOURT_DECLEMINATION:
                 switch (k) {
                     case 14:
                         System.out.println("Мне " + k + " лет");
