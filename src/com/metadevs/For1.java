66f327833b5b90b63855bcadea2917d2c8a9e9e8
shh <vi950424@gmail.com>
2022-04-28 19:23:09 +0300

    for1 task done

--- src/com/metadevs/For1.java
+++ src/com/metadevs/For1.java
@@ -1,9 +1,28 @@
 package com.metadevs;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 //задача №1 со stepic раздел 7.3
 public class For1 {
 
     public static void main(String[] args) {
-
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите количество элементов.");
+            int size = scan.nextInt();
+            int array[] = new int[size];
+            System.out.println("Введите элементы.");
+            for (int i = 0; i < size; i++) {
+                array[i] = scan.nextInt();
+            }
+            System.out.println("Элементы в обратном порядке.");
+            for (int i = size - 1; i >= 0; i--) {
+                array[0] = 0;
+                if (array[i] > 0) {
+                    System.out.print(array[i] + " ");
+                }
+            }
+        }
     }
-}
+
---
+++ src/com/metadevs/livecodingoop/Main.java
@@ -0,0 +1,4 @@
+package com.metadevs.livecodingoop;
+
+public class Main {
+}
