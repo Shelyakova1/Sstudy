b547ba6982e8646e72c3c221da0591b225f80f22
shh <vi950424@gmail.com>
2022-06-07 20:03:46 +0300

    task stepic

--- src/com/metadevs/For3.java
+++ src/com/metadevs/For3.java
@@ -1,5 +1,26 @@
 package com.metadevs;
 
+import java.util.Scanner;
+
 //задача №3 со stepic раздел 7.3
 public class For3 {
+    public static void main(String[] args) {
+        Scanner scanNum = new Scanner(System.in);
+
+        int mum;
+        boolean input = true;
+        do {
+            mum = scanNum.nextInt();
+            if (mum > 0 || mum == -9999) {
+
+                input = false;
+            }
+        }
+
+        while (mum != -9999);
+        System.out.println(input ? "YES" : "NO");
+    }
+
 }
+
+
