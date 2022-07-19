d6e2a5c4d74b886a2a590c607615d45da910660c
shh <vi950424@gmail.com>
2022-07-19 20:48:28 +0300

    rebase master

--- src/main/java/com/metadevs/tfp/for/Task3b.java
+++ src/main/java/com/metadevs/tfp/for/Task3b.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task3b {
+public class Task3b{
     public static void main(String[] args) {
         int numbersForCheck[] = {5, 0, -3, 2};
         for (int i = 0; i < numbersForCheck.length; i++) {
